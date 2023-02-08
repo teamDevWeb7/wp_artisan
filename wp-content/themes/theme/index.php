104c0de2dc6f8d8974b8079712e5bc95e19666f0
teamDevWeb7 <suray.amelyne@gmail.com>
2023-02-08 08:39:45 +0100

    first commit

---
+++ wp-content/themes/theme/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="./style/style1.css">
+</head>
+<body>
+
+    <header>
+        <ul>
+            <li><a href="./index.php"> <img src="./imgs/pug-dog-isolated-white-background.jpg" alt="image chien"> </a></li>
+            <li> <a href="./index.php" id="aAcc">Accueil</a>  </li>
+            <li><a href="./prest.php" id="aServ">Services</a></li>
+            <li><a href="./pres.php" id="aDec">Nous découvrir</a></li>
+            <li><a href="./contact.php" id="aCon">Contact</a></li>
+            <li><a href="./connexion.php"> <img src="./imgs/head.png" alt="image connexion"> </a></li>
+        </ul>
+    </header>
+
+    <div id="intro">
+        <h1>Friend-Sitter</h1>
+    </div>
+    <div id="test"></div>
+
+
+
+    
+</body>
+</html>
