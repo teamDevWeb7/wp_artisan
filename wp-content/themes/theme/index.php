b5091a6ea1976469fb8d2f69086272812dfbddcc
teamDevWeb7 <suray.amelyne@gmail.com>
2023-02-09 08:36:53 +0100

    html et css accueil

--- wp-content/themes/theme/index.php
+++ wp-content/themes/theme/index.php
@@ -26,15 +26,77 @@
     </div>
     <main>
         <svg height="600" width="1263">
-            <polygon points="0,0 0,600 1263,400 1263,0"/>
+            <polygon style="fill:rgb(255, 230, 0);" points="0,0 0,600 1263,400 1263,0"/>
         </svg>
 
-
-        <div id="forme1">
-
+        <h2>Bienvenue chez Friend-Sitter !</h2>
+        <div id="p1">
+            <p>Vous avez besoin d'animaux pour vos story insta mais vous n'aimez pas vous en occuper ?</p>
+            <p>Nous avons la solution !</p>
+            <p>A un prix très compétitif nos gardons vos animaux :)</p>
+            <a href="./pres.php">Découvrir l'équipe</a>
         </div>
+        <img src="./imgs/dog-g799fb2696_1920.jpg" alt="image décorative" id="g7">
+
+        <h3>Découvrir toutes nos activités</h3>
+        <div id="ateliers">
+            <div>
+                <a href="./prest.php">Atelier introspection</a>
+                <img src="./imgs/friends-g4f771af41_1920.jpg" alt="chien et humain pensifs">
+            </div>
+            <div>
+                <a href="./prest.php">Atelier sieste</a>
+                <img src="./imgs/girl-g8b4e879a2_1920.jpg" alt="chien et humain à la sieste">
+            </div>
+            <div>
+                <a href="./prest.php">Atelier team building</a>
+                <img src="./imgs/girl-g41a984c28_1920.jpg" alt="chien et humain jouent">
+            </div>
+            <div>
+                <a href="./prest.php">Atelier savoir vivre</a>
+                <img src="./imgs/golden-retriever-puppy-gab287e0f0_1920.jpg" alt="chien mange propre ">
+            </div>
+        </div>
+
+
+        
+        <svg height="600" width="1263">
+            <polygon style="fill:rgb(9, 216, 243);" points="0,50 0,1000 1263,1000 1263,200"/>
+        </svg>
+
+        <h4>Captivés ?</h4>
+        <div id="contact">
+            <a href="./contact.php">Contactez-nous pour plus d'informations</a>
+            <div>
+                <img src="./imgs/boites-aux-lettres.png" alt="lettre">
+                <img src="./imgs/old-phone.png" alt="lettre">
+                <img src="./imgs/e-mail.png" alt="lettre">
+            </div>
+        </div>
+        
+
     </main>
-    <div id="test"></div>
+    <footer>
+        <nav id="navouille">
+            <ul>
+                <li> <a href="./index.php" id="aAcc">Accueil</a>  </li>
+                <li><a href="./prest.php" id="aServ">Services</a></li>
+                <li><a href="./pres.php" id="aDec">Nous découvrir</a></li>
+                <li><a href="./contact.php" id="aCon">Contact</a></li>
+            </ul>
+        </nav>
+        <nav id="reseaux">
+            <a href="#"><img src="./imgs/facebook.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/instagram.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/github.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/discord.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/tik-tok.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/youtube.png" alt="icone reseau"></a>
+            <a href="#"><img src="./imgs/twitter.png" alt="icone reseau"></a>
+
+        </nav>
+        <p>c 2023 ~ Tous droits réservés à Friend-Sitter</p>
+    </footer>
 
 
 
